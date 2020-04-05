d61164327ad4b5ee9506fc80355231ec7e2d8225
Thushitha-Madusanka <thusithamadusanka92@gmail.com>
2020-04-05 13:04:36 +0530

    Initial commit

---
+++ app/src/main/java/com/tmsoft/food_delevery/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tmsoft.food_delevery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
